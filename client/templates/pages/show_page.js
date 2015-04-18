0faea7d5fe6baa568f8ce28151f2ca20f878fb21
Albert Kurniawan <kurniawan.albert19@gmail.com>
2015-04-19 01:48:36 +1000

    fixed layout in opening_hourse wit response

--- client/templates/pages/show_page.js
+++ client/templates/pages/show_page.js
@@ -97,30 +97,54 @@ Template.showPage.events({
           alert(this.facebook.about);
           break;
         case "opening_hours":
-          var toPrint = 'We are open on:<br/>';
           if (!res.day) res.day = 'all';
-          console.log(res.day);
-          if (res.day == 'monday' || res.day == 'all')
-            toPrint += 'Monday: '+facebookHours.mon_1_open+' - '+facebookHours.mon_1_close+'<br/>';
-          if (res.day == 'tuesday' || res.day == 'all')
-            toPrint += 'Tuesday: '+facebookHours.tue_1_open+' - '+facebookHours.tue_1_close+'<br/>';
-          if (res.day == 'wednesday' || res.day == 'all')
-            toPrint += 'Wednesday: '+facebookHours.wed_1_open+' - '+facebookHours.wed_1_close+'<br/>';
-          if (res.day == 'thursday' || res.day == 'all')
-            toPrint += 'Thursday: '+facebookHours.thu_1_open+' - '+facebookHours.thu_1_close+'<br/>';
-          if (res.day == 'friday' || res.day == 'all')
-            toPrint += 'Friday: '+facebookHours.fri_1_open+' - '+facebookHours.fri_1_close+'<br/>';
-          if (res.day == 'saturday' || res.day == 'all')
-            toPrint += 'Saturday: '+facebookHours.sat_1_open+' - '+facebookHours.sat_1_close+'<br/>';
-          if (res.day == 'sunday' || res.day == 'all')
-            toPrint += 'Sunday: '+facebookHours.sun_1_open+' - '+facebookHours.sun_1_close+'<br/>';
-          toPrint += '<br/>Woof!'
-          addWitResponseAI(toPrint);
+          var toPrint = '';
+          if (res.day == 'monday' || res.day == 'all') {
+            toPrint += 'Monday: '+facebookHours.mon_1_open+' - '+facebookHours.mon_1_close;
+            if (res.day == 'all') toPrint += '<br/>';
+          }
+
+          if (res.day == 'tuesday' || res.day == 'all') {
+            toPrint += 'Tuesday: '+facebookHours.tue_1_open+' - '+facebookHours.tue_1_close;
+            if (res.day == 'all') toPrint += '<br/>';
+          }
+
+          if (res.day == 'wednesday' || res.day == 'all') {
+            toPrint += 'Wednesday: '+facebookHours.wed_1_open+' - '+facebookHours.wed_1_close;
+            if (res.day == 'all') toPrint += '<br/>';
+          }
+
+          if (res.day == 'thursday' || res.day == 'all') {
+            toPrint += 'Thursday: '+facebookHours.thu_1_open+' - '+facebookHours.thu_1_close;
+            if (res.day == 'all') toPrint += '<br/>';
+          }
+
+          if (res.day == 'friday' || res.day == 'all') {
+            toPrint += 'Friday: '+facebookHours.fri_1_open+' - '+facebookHours.fri_1_close;
+            if (res.day == 'all') toPrint += '<br/>';
+          }
+
+          if (res.day == 'saturday' || res.day == 'all') {
+            toPrint += 'Saturday: '+facebookHours.sat_1_open+' - '+facebookHours.sat_1_close;
+            if (res.day == 'all') toPrint += '<br/>';
+          }
+
+          if (res.day == 'sunday' || res.day == 'all') {
+            toPrint += 'Sunday: '+facebookHours.sun_1_open+' - '+facebookHours.sun_1_close;
+            if (res.day == 'all') toPrint += '<br/>';
+          }
+
+          var finalString = '';
+          if (res.day == 'all')
+            finalString = 'We are open on:<br/>' + toPrint + '<br/>Woof!';
+          else
+            finalString = 'On ' + toPrint + ', Woof!';
+
+          addWitResponseAI(finalString);
           break;
         }
     });
-
-
+    $(e.target).find('[name=query]').val('');
   }
 
 })
